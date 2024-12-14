a58de40fff134cf18a85b8c92f9622ed3ff62577
Bernhard Rosenkränzer <bero@lindev.ch>
2024-12-14 21:23:17 +0100

    5.74, fix auto-update

--- check-update.sh
+++ check-update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s -L -A 'Mozilla/5.0 (X11; Linux x86_64)' http://www.stunnel.org/downloads.html |grep -E "href=.*stunnel-" |sed -e "s,.*stunnel-,,;s,\.tar.*,," | tail -n1
+curl -s -L -A 'Mozilla/5.0 (X11; Linux x86_64)' http://www.stunnel.org/downloads.html |grep -E "href=.*stunnel-" |grep -v latest |sed -e "s,.*stunnel-,,;s,\.tar.*,," | tail -n1
--- stunnel.spec
+++ stunnel.spec
@@ -2,7 +2,7 @@
 
 Summary:	Program that wraps normal socket connections with SSL/TLS
 Name:		stunnel
-Version:	5.70
+Version:	5.74
 Release:	1
 License:	GPLv2
 Group:		System/Servers
